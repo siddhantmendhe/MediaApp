8cbc6a0857a49266659d354f6daace613b44d73e
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-08 20:55:43 +0530

    json server created

--- mediaapp/src/App.js
+++ mediaapp/src/App.js
@@ -1,7 +1,11 @@
+import UsersList from "./components/UsersList";
+
 function App(){
-    return <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>;
+    return <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">Hello world!
+  </h1>
+  <UsersList/>
+  </div>;
 }
 
 export default App;
