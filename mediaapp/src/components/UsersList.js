12c217f953075c9b59aeacafddabbe754aa68c1b
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-10 20:19:02 +0530

    added data display

--- mediaapp/src/components/UsersList.js
+++ mediaapp/src/components/UsersList.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch,useSelector } from "react-redux";
 import Skeleton from "./Skeleton";
 import { fetchUsers } from "../store";
+import { render } from "@testing-library/react";
 
 function UsersList(){
     const dispatch=useDispatch();
@@ -20,8 +21,15 @@ function UsersList(){
         return <div>Error fetching data...</div>
     }
     
+    const renderedUsers=data.map((user)=>{
+        return <div key={user.id} className="mb-2 border rounded">
+            <div className="flex p-2 justify-between items-center cursor-pointer">
+                {user.name}
+            </div>
+        </div>
+    })
     return <div>
-        {data.length}
+        {renderedUsers}
     </div>
 }
 
