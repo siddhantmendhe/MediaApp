8a4e08f87832afb1f149cac0d272c3bec93be714
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-13 15:47:54 +0530

    remove user feature added

--- mediaapp/src/components/UsersList.js
+++ mediaapp/src/components/UsersList.js
@@ -4,6 +4,7 @@ import Skeleton from "./Skeleton";
 import { fetchUsers,addUser } from "../store";
 import Button from './Button'
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 
 function UsersList(){
@@ -30,11 +31,8 @@ function UsersList(){
     }
     else{
         content=data.map((user)=>{
-            return <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    {user.name}
-                </div>
-            </div>
+            return <UsersListItem key={user.id} user={user}/>
+           
         })
     }
     
