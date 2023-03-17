a96fdf36841e710752b13e929c0437ebb4554ba8
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-17 13:06:22 +0530

    using generated hooks

--- mediaapp/src/components/AlbumsList.js
+++ mediaapp/src/components/AlbumsList.js
@@ -1,4 +1,11 @@
-function AlbumsList({user}){
-    return <div>Albums for {user.name}</div>;
+import { useFetchAlbumsQuery } from '../store';
+
+function AlbumsList({ user }) {
+const {data,isError, isLoading}= useFetchAlbumsQuery(user);
+
+  console.log(data,isLoading,isError)
+
+  return <div>Albums for {user.name}</div>;
 }
+
 export default AlbumsList;
--- mediaapp/src/store/apis/albumsApi.js
+++ mediaapp/src/store/apis/albumsApi.js
@@ -1,28 +1,26 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const albumsApi=createApi({
-    reducerPath:'albums',
-    baseQuery:fetchBaseQuery({
-        baseUrl:'http://localhost:3005/',
-    }),
-    endpoints(builder){
-        return{
-            fetchAlbums:builder.query({
-                query:(user)=>{
-                    return{
-                        url:'albums',
-                        params:{
-                           userId: user.id,
-                        },
-                        method:'GET',
-                    }
+const albumsApi = createApi({
+  reducerPath: 'albums',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3005',
+  }),
+  endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: '/albums',
+            params: {
+              userId: user.id,
+            },
+            method: 'GET',
+          };
+        },
+      }),
+    };
+  },
+});
 
-                }
-            })
-
-        };
-    }
-})
-
-export const {useFetchAlbumsQuery}= albumsApi;
-export {albumsApi};
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi };
