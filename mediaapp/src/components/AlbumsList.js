3c7626b79b6b33ef80044d360e843434e04df89e
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-22 13:33:12 +0530

    mutation hook created

--- mediaapp/src/components/AlbumsList.js
+++ mediaapp/src/components/AlbumsList.js
@@ -1,11 +1,42 @@
-import { useFetchAlbumsQuery } from '../store';
-
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
+import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+import Skeleton from './Skeleton';
 function AlbumsList({ user }) {
-const {data,isError, isLoading}= useFetchAlbumsQuery(user);
+  const {data,isError, isLoading}= useFetchAlbumsQuery(user);
+  const [addAlbum,results]=useAddAlbumMutation();
+  console.log(data)
+
+  const handleAddAlbum=()=>{
+    addAlbum(user);
+  }
+  let content;
+  if(isLoading){
+    content=<Skeleton times={3}/>
+  }
+  else if(isError){
+    content=<div>Error loading albums.</div>
+  } 
+  else{
+    content=data.map(album=>{
+      const header=<div>{album.title}</div>
+      return <ExpandablePanel key={album.id} header={header}>
+        List of photos in the album
+        </ExpandablePanel>
+    })
+  }
 
   console.log(data,isLoading,isError)
 
-  return <div>Albums for {user.name}</div>;
+  return <div>
+    <div>Albums for {user.name}
+    <Button onClick={handleAddAlbum}>
+      + Add Album
+
+    </Button>
+    </div>
+    <div>{content}</div>
+    </div>;
 }
 
 export default AlbumsList;
