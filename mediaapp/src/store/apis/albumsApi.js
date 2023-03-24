3ef1bde38a4c4a2e32f9843295f0426df9c505c0
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-03-24 18:41:33 +0530

    Implemented Tags

--- mediaapp/src/store/apis/albumsApi.js
+++ mediaapp/src/store/apis/albumsApi.js
@@ -9,6 +9,7 @@ const albumsApi = createApi({
   endpoints(builder) {
     return {
       addAlbum:builder.mutation({
+        invalidatesTags:['Album'],
         query:(user)=>{
           return{
             url:'/albums',
@@ -23,6 +24,7 @@ const albumsApi = createApi({
         }
       }),
       fetchAlbums: builder.query({
+        providesTags:['Album'],
         query: (user) => {
           return {
             url: '/albums',
